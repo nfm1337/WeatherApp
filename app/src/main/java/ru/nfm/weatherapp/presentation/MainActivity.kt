b3a6a1041176e32cc49a322edc045e4fd1917825
NFM <daniel.olovianniy@gmail.com>
2024-04-18 02:25:33 +0300

    main screen design

--- app/src/main/java/ru/nfm/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/nfm/weatherapp/presentation/MainActivity.kt
@@ -1,18 +1,12 @@
 package ru.nfm.weatherapp.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.arkivanov.decompose.defaultComponentContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import ru.nfm.weatherapp.WeatherApp
-import ru.nfm.weatherapp.data.network.api.ApiFactory
 import ru.nfm.weatherapp.presentation.root.DefaultRootComponent
 import ru.nfm.weatherapp.presentation.root.RootContent
-import ru.nfm.weatherapp.presentation.ui.theme.WeatherAppTheme
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
